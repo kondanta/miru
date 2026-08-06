f1fd8ce71de9d05c9e6956b47d38e2cc847dd5e3
Taylan Dogan <git@taylan.dev>
2026-08-06 18:05:16 +0200

    feat(db): add initial schema migration
    Signed-off-by: Taylan Dogan <git@taylan.dev>

---
+++ migrations/000001_initial_schema.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS jellyfin_configs;
+DROP TABLE IF EXISTS webhooks;
+DROP INDEX IF EXISTS downloads_status;
+DROP INDEX IF EXISTS downloads_user_id;
+DROP TABLE IF EXISTS downloads;
+DROP TABLE IF EXISTS watch_later_configs;
+DROP TABLE IF EXISTS youtube_tokens;
+DROP TABLE IF EXISTS users;
