4b74835ea192ee117ccd9e091dff42e3736c505d
Taylan Dogan <git@taylan.dev>
2026-08-06 02:36:23 +0200

    feat: add renovate
    Signed-off-by: Taylan Dogan <git@taylan.dev>

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 //	go build -ldflags "-X github.com/kondanta/miru/cmd.version=1.2.3"
 var version = "dev"
 
-// Execute builds and runs the miru command tree, displaying help when no subcommand is provided and supporting version output.
+// Execute builds and runs the miru command tree.
 func Execute() {
 	rootCmd := &cobra.Command{
 		Use:           "miru",
